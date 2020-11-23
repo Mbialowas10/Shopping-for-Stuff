c86c2f35d1b631dab7779490db6823be0e26d117
Kyle Geske <stungeye@gmail.com>
2020-11-23 11:33:02 -0600

    New routes and a checkout controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :products, only: %i[index show]
+
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+    get "success", to: "checkout#success", as: "checkout_sucess"
+  end
+
   root to: "products#index"
 end

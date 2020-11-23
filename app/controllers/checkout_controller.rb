ccfd07d7467d788e2035d94433c0424f85830483
Kyle Geske <stungeye@gmail.com>
2020-11-23 11:48:54 -0600

    Create button that triggers a js view.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,13 @@
 class CheckoutController < ApplicationController
+  def create
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.js # render create.js.erb
+    end
+  end
+
+  def success; end
+
+  def cancel; end
 end

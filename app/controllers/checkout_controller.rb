38cd5e265dcb0d179f5b8f0c3e3b96de65563dbe
Kyle Geske <stungeye@gmail.com>
2020-11-23 15:03:28 -0600

    Completed the stripe test.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,13 +1,34 @@
 class CheckoutController < ApplicationController
   def create
-    @product = Product.find(params[:id])
+    product = Product.find(params[:product_id])
+
+    if product.nil?
+      redirect_to root_path
+      return
+    end
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      line_items:           [{
+        name:        product.name,
+        description: product.description,
+        amount:      product.price_cents,
+        currency:    "cad",
+        quantity:    1
+      }],
+      success_url:          checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url:           checkout_cancel_url
+    )
 
     respond_to do |format|
       format.js # render create.js.erb
     end
   end
 
-  def success; end
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  end
 
   def cancel; end
 end

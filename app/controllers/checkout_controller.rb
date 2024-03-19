4191910fe6af3446aff53c23c1da07984e7443b8
Mbialowas10 <94670314+Mbialowas10@users.noreply.github.com>
2024-03-19 09:50:02 -0500

    Stripe integration march 19, 2024 s251426

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -9,23 +9,53 @@ class CheckoutController < ApplicationController
       return
     end
 
-
-
     @session = Stripe::Checkout::Session.create(
-      payment_method_types: ["card"],
-      success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: checkout_cancel_url,
-      line_items: [
+  line_items: [{
+    price_data: {
+      currency: 'cad',
+      unit_amount: product.price_cents,
+      product_data: {
         name: product.name,
-        description: product.description,
-        amount: product.price_cents, #price * 100.to_i
-        currency: "cad",
-        quantity: 1
-      ]
-    )
-    respond_to do | format |
-      format.js #app/views/checkout/create.js.erb
-    end
+        description: product.description
+
+      },
+    },
+    quantity: 1,
+  }],
+  mode: 'payment',
+  success_url: 'checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}',
+  cancel_url: 'checkout_cancel_url',
+)
+
+  #   @session = Stripe::Checkout::Session.create(
+
+  #     line_items: [
+  #       {
+  #         name: product.name,
+  #         description: product.description,
+  #         unit_amount: product.price_cents, #price * 100.to_i
+  #         currency: "cad",
+  #         quantity: 1
+  # },
+  #       price_data: {
+  #           name: "GST",
+  #           description: "Good and Services Tax",
+  #           unit_amount: (product.price_cents * 0.05).to_i,
+  #           currency: "cad",
+
+  #       },
+  #     quantity: 1
+  #   ],
+  #     payment_method_types: ["card"],
+  #     success_url: checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
+  #     cancel_url: checkout_cancel_url,
+  #   )
+
+
+    # respond_to do | format |
+    #   format.js #app/views/checkout/create.js.erb
+    # end
+    redirect_to @session.url, allow_other_host: true
   end
 
   def success
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   # /checkout/create something
   scope "/checkout" do
-    post "create", to: "checkout#create", as: "checkout_create"
-    get "success", to: "checkout#success", as: "checkout_success"
-    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+    post "create", to: "checkout#create", as: "checkout_create" # sending the data off to stripe
+    get "success", to: "checkout#success", as: "checkout_success" # recieving the data from stripe until successfull transaction
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel" # not receiving the data back from stripe
   end
 end

76b25e9ec5e0727db63e74e4a682733557aed303
Mbialowas10 <94670314+Mbialowas10@users.noreply.github.com>
2024-03-20 15:26:10 -0500

    Blank Canvas

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
   resources :products, only: %i[index show]
   root to: "products#index"
 
-  resources :cart, only: [:create, :destroy]
 
-  # /checkout/create something
-  scope "/checkout" do
-    post "create", to: "checkout#create", as: "checkout_create"
-    get "success", to: "checkout#success", as: "checkout_success"
-    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
-  end
+
+
 end
